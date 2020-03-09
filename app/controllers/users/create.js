5fff379bd499353e8024b19f2e9c94e4f30ccdbe
Alicia <alicia@ringover.com>
2020-03-10 00:26:36 +0100

    POST user => double mail control

--- app/controllers/users/create.js
+++ app/controllers/users/create.js
@@ -19,18 +19,27 @@ class Create {
     this.app.post('/user/create', (req, res) => {
       try {
         const userModel = new this.UserModel(req.body)
-
-        userModel.save().then(user => {
-          res.status(200).json(user || {})
-        }).catch(err => {
-          res.status(500).json({
-            'code': 500,
-            'message': err
-          })
+        this.UserModel.findOne({ email: userModel.email }, (err, user) => {
+          if (user !== null) {
+            res.status(500).json({
+              'message': 'Email already used !'
+            })
+          } else {
+            userModel.save().then(user => {
+              res.status(200).json(user || {})
+            }).catch(err => {
+              res.status(500).json({
+                'code': 500,
+                'message': err
+              })
+            })
+          }
+          if (err) {
+            console.log(err)
+          }
         })
       } catch (err) {
         res.status(500).json({
-          'code': 500,
           'message': err
         })
       }
@@ -39,7 +48,8 @@ class Create {
 
   /**
    * run
-   */
+  **/
+ 
   run () {
     this.middleware()
   }
